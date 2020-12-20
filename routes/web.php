a259ac2014a093d7ddfb00f66503775d4371b7fd
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2020-12-19 23:39:35 -0300

    Adicionando rota para a página inicial do sistema

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function() {
+    return view('welcome');
+})->name('home');
+
 Route::prefix('/admin')->group(function() {
     Route::get('/index', function() {
         return 'Admin listing route';
