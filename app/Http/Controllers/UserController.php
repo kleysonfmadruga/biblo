d0617b362a9fc7c90ec27a187a40d0d6d17ddc55
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2020-12-20 21:55:03 -0300

    Criando regra de negócio para criação e autenticação de usuário e exibição da página de cadastro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use App\Models\User;
+use Validator;
 
 class UserController extends Controller
 {
@@ -16,14 +21,38 @@ class UserController extends Controller
         //
     }
 
+    public function auth(Request $request){
+        $validation = Validator::make($request->all(), [
+            'email' => ['required','email'],
+            'passwd' => ['required','min:6'],
+        ]);
+
+        if($validation->fails()){
+            return back()->withErrors($validation);
+        }
+
+        $auth_data = array(
+            'email' => $request->email,
+            'password' => $request->passwd
+        );
+
+        if(Auth::attempt($auth_data)){
+            return redirect()->route('index');
+        } else {
+            return back()->withErrors(["login", "Usuário e/ou senha incorretos. Por favor, tente novamente."]);
+        }
+
+    }
+
     /**
+     * 
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('signup');
     }
 
     /**
@@ -34,7 +63,32 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $validation = Validator::make($request->all(), [
+            'name' => ['required','max:255'],
+            'email' => ['required','email'],
+            'passwd' => ['required','min:6'],
+            'passwd_confirm' => ['required','same:passwd']
+        ]);
+
+        if($validation->fails()){
+            return back()->withErrors($validation);
+        }
+
+        $user = new User();
+        //$user->name = $request->name;
+        //$user->email = $request->email;
+        //$user->password = Hash::make($request->passwd);
+        //$user->photo = storage_path('profile_img/generic-avatar.png');
+        $user->create([
+            'id' => Str::uuid(),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->passwd),
+            'photo' => storage_path('profile_img/generic-avatar.png'),
+        ]);
+
+        return redirect()->route('home');
     }
 
     /**
