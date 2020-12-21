6399b90270e4c0e38b26dbda57ba65a5b6519181
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2020-12-20 23:47:01 -0300

    Adicionando função para deslogar usuário do sistema

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,6 +44,16 @@ class UserController extends Controller
 
     }
 
+    public function logout(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+
     /**
      * 
      * Show the form for creating a new resource.
