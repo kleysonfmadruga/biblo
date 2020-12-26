1511f613722e1f8cc2b853a0695f089dcb621fca
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2020-12-25 23:50:33 -0300

    feat(index): Adiciona view index renderizada dinamicamente

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,117 @@ class UserController extends Controller
     public function index()
     {   
         $user = Auth::user();
-        return view('index', array('user' => $user));
+        
+        $book_list_1 = [
+            [
+                "link" => "#",
+                "name" => "Harry Potter e a Pedra Filosofal",
+                "cover" => "harry-potter-e-a-pedra-filosofal.jpeg"
+            ],
+            [
+                "link" => "#",
+                "name" => "Harry Potter e o Enigma do Príncipe",
+                "cover" => "harry-potter-e-o-enigma-do-principe.jpeg"
+            ],
+            [
+                "link" => "#",
+                "name" => "Harry Potter e a Ordem da Fênix",
+                "cover" => "harry-potter-e-a-ordem-da-fenix.jpeg"
+            ],
+            [
+                "link" => "#",
+                "name" => "Harry Potter e as Relíquias da Morte",
+                "cover" => "harry-potter-e-as-reliquias-da-morte.jpeg"
+            ]
+        ];
+
+        $book_list_2 = [
+            [
+                "link" => "#",
+                "name" => "Dom Quixote de la Mancha",
+                "cover" => "dom-quixote-de-la-mancha.jpeg"
+            ],
+            [
+                "link" => "#",
+                "name" => "Romeu e Julieta",
+                "cover" => "romeu-e-julieta.jpeg"
+            ],
+            [
+                "link" => "#",
+                "name" => "O Pequeno Príncipe",
+                "cover" => "o-pequeno-principe.jpeg"
+            ],
+            [
+                "link" => "#",
+                "name" => "A Metamorfose",
+                "cover" => "a-metamorfose.jpeg"
+            ]
+        ];
+
+        $selection_list_1 = [
+            [
+                "link" => "#",
+                "title_sup" => "Novo no Biblo?",
+                "title_sub" => "Comece por aqui &#x1F609;",
+                "grad_direction" => "tr",
+                "grad_from" => "yellow-400",
+                "grad_to" => "purple-500"
+            ],
+            [
+                "link" => "#",
+                "title_sup" => "Cuidado com",
+                "title_sub" => "o que espreita nas sombras... &#x1F47B;",
+                "grad_direction" => "b",
+                "grad_from" => "indigo-500",
+                "grad_to" => "black"
+            ],
+            [
+                "link" => "#",
+                "title_sup" => "Preguiça de escolher?",
+                "title_sub" => "Vamos dar um rolê aleatório! &#x1F929;",
+                "grad_direction" => "tl",
+                "grad_from" => "green-600",
+                "grad_to" => "yellow-400"
+            ]
+        ];
+
+        $selection_list_2 = [
+            [
+                "link" => "#",
+                "title_sup" => "Roi rs. Letícia, né?",
+                "title_sub" => "Gostaria de um romance? &#x1F60D;",
+                "grad_direction" => "br",
+                "grad_from" => "pink-300",
+                "grad_to" => "red-600"
+            ],
+            [
+                "link" => "#",
+                "title_sup" => "As fadas existem",
+                "title_sub" => "e eu posso provar! &#x1F9DA;&#x2728;",
+                "grad_direction" => "r",
+                "grad_from" => "purple-400",
+                "grad_to" => "pink-600"
+            ],
+            [
+                "link" => "#",
+                "title_sup" => "Venha, aventureiro!",
+                "title_sub" => "Vamos desbravar esta selva &#x1F920;",
+                "grad_direction" => "bl",
+                "grad_from" => "green-300",
+                "grad_to" => "green-700"
+            ]
+        ];
+
+        return view(
+            'index',
+            array(
+                'user' => $user,
+                'book_list_1' => $book_list_1,
+                'book_list_2' => $book_list_2,
+                'selection_list_1' => $selection_list_1,
+                'selection_list_2' => $selection_list_2
+            )
+        );
     }
 
     public function auth(Request $request){
@@ -87,16 +197,13 @@ class UserController extends Controller
         }
 
         $user = new User();
-        //$user->name = $request->name;
-        //$user->email = $request->email;
-        //$user->password = Hash::make($request->passwd);
-        //$user->photo = storage_path('profile_img/generic-avatar.png');
+
         $user->create([
             'id' => Str::uuid(),
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->passwd),
-            'photo' => url('storage/profile_img/generic-avatar.png'),
+            'photo' => 'storage/profile_img/generic-avatar.png',
         ]);
 
         return redirect()->route('login');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,58 @@
 @endsection
 
 @section('main')
+    <article class="bg-gray-50 py-6 px-16">
+        <section class="flex flex-row justify-evenly">
+            
+            @foreach($selection_list_1 as $sel)
+                <a class="flex flex-col px-4 py-8 mb-2 w-72 h-48 bg-gradient-to-{{$sel['grad_direction']}} from-{{$sel['grad_from']}} to-{{$sel['grad_to']}} rounded-md hover:z-10 shadow-md transform hover:rotate-1 hover:shadow-lg justify-end duration-200 group" href="#">
+                    <h3 class="text-lg text-white duration-200 group-hover:underline group-hover:rotate-1">{!!$sel['title_sup']!!}</h3>
+                    <h3 class="text-lg text-white duration-200 group-hover:underline group-hover:rotate-1">{!!$sel['title_sub']!!}</h3>
+                </a>
+            @endforeach
+        
+        </section>
+        <section class="flex flex-col mt-6 pb-6">
+            <hr class="border-gray-300"/>
+            <h3 class="text-xl font-bold text-gray-900 h-12 mt-4">Seleção do dia: um mundo de magia e uma escola de bruxaria &#x1F9D9;</h3>
+            <div class="flex flex-row justify-evenly items-center mb-4">
+        
+                @foreach ($book_list_1 as $book)
+                    <a href={{url($book['link'])}} class="flex flex-col w-52 p-4 justify-center rounded-md hover:bg-gray-200 hover:z-10 hover:shadow-md duration-200">
+                        <img src={{url("storage/book_covers/mini/{$book['cover']}")}} alt="Livro" class="h-64"/>
+                        <p class="mt-2 truncate">{{$book['name']}}</p>
+                    </a>
+                @endforeach
+        
+                <a href="#" class="p-2 bg-gray-100 hover:bg-gray-200 rounded hover:z-10 hover:shadow duration-200">Ver mais</a>
+            </div>
+            <hr class="border-gray-300" />
+        </section>
+        <section class="flex flex-row justify-evenly">
+        
+            @foreach($selection_list_2 as $sel)
+                <a class="flex flex-col px-4 py-8 mb-2 w-72 h-48 bg-gradient-to-{{$sel['grad_direction']}} from-{{$sel['grad_from']}} to-{{$sel['grad_to']}} transform hover:rotate-1 rounded-md hover:z-10 shadow-md hover:shadow-lg justify-end duration-200 group" href="#">
+                    <h3 class="text-lg text-white duration-200 group-hover:underline group-hover:rotate-1">{!!$sel['title_sup']!!}</h3>
+                    <h3 class="text-lg text-white duration-200 group-hover:underline group-hover:rotate-1">{!!$sel['title_sub']!!}</h3>
+                </a>
+            @endforeach
 
+        </section>
+        <section class="flex flex-col mt-6 pb-6">
+            <hr class="border-gray-300"/>
+            <h3 class="text-xl font-bold text-gray-900 h-12 mt-4">Os clássicos que nunca saem de moda &#x1F913;</h3>
+            <div class="flex flex-row justify-evenly items-center mb-4">
+            
+                @foreach ($book_list_2 as $book)
+                    <a href={{url($book['link'])}} class="flex flex-col w-52 p-4 justify-center rounded-md hover:bg-gray-200 hover:z-10 hover:shadow-md duration-200">
+                        <img src={{url("storage/book_covers/mini/{$book['cover']}")}} alt="Livro" class="h-64"/>
+                        <p class="mt-2 truncate">{{$book['name']}}</p>
+                    </a>
+                @endforeach
+            
+                <a href="#" class="p-2 bg-gray-100 hover:bg-gray-200 rounded hover:z-10 hover:shadow duration-200">Ver mais</a>
+            </div>
+            <hr class="border-gray-300" />
+        </section>
+    </article>
 @endsection
